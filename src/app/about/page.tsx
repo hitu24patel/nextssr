1d3a52294f690478c34a0f0871cbc1e2261ee299
Hitesh Patel <hitu.24patel@gmail.com>
2024-10-18 16:50:25 +0530

    ui

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,16 @@
+import type { Metadata } from "next";
 
-import type { Metadata } from 'next'
- 
 export const metadata: Metadata = {
-  title: 'This is about page lodo title',
-  description: 'This is about page pikki description',
+  title: "This is about page title",
+  description: "This is about page description",
+};
+
+export default function About() {
+  return (
+    <>
+      <h1>Where does it come from?</h1>
+      <p>I am from Junagardh</p>
+      <p>I am from Junagardh, Girnar, Porbandar</p>
+    </>
+  );
 }
- 
-export default function About() {}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,16 @@
+import type { Metadata } from "next";
 
-import type { Metadata } from 'next'
- 
 export const metadata: Metadata = {
-  title: 'This is Contact page lodo title',
-  description: 'This is Contact page pikki description',
+  title: "This is Contact page title",
+  description: "This is Contact page description",
+};
+
+export default function Contact() {
+  return (
+    <>
+      <h1>Where does it come from?</h1>
+      <p>I am from Idar</p>
+      <p>I am from Idar, Sabarkantha, Himmatnagar</p>
+    </>
+  );
 }
- 
-export default function Contact() {}
