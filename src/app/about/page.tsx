42e3046a53e3636ca428d8908f0a38b5290c45dd
Hitesh Patel <hitu.24patel@gmail.com>
2024-10-18 16:42:25 +0530

    added project code

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'This is about page lodo title',
+  description: 'This is about page pikki description',
+}
+ 
+export default function About() {}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'This is Contact page lodo title',
+  description: 'This is Contact page pikki description',
+}
+ 
+export default function Contact() {}
